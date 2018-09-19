46251ba060f48659332a61e3dae29e9d39eaccb8
Sergey Volynez <svolynez@marketingg2.com>
2018-09-19 12:30:45 +0300

    created regular expression for URL og article

--- public/js/Connext.js
+++ public/js/Connext.js
@@ -7049,7 +7049,7 @@ var ConnextUtils = function ($) {
             return diff;
         },
         GetUrl: function () {
-            return location.protocol + "//" + location.host + location.pathname + location.hash;
+            return location.href.match(/.+?(?=[\/])[^?# ]+/g) == null ? location.protocol + "//" + location.host + location.pathname : location.href.match(/.+?(?=[\/])[^?# ]+/g).join('');
         },
         GetHiddenFormFieldValue: function (selector) { //we take any jquery selector, so it can be a class, id, data atrribute etc...
             try {
